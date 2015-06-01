136ee7090f59229168622d44bd12772f8978a51b
Rzymon <panmiska@gmail.com>
2015-06-01 20:20:06 +0200

    shuffle

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "Card.h"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
 
 int main()
 {
+    srand(time(NULL));
     Card *cards = Card::deck();
 
     for ( int i = 0 ; i < 52 ; i++ )
@@ -10,5 +15,11 @@ int main()
 
     printf("\nOto karty były\n");
 
+    for (int k=0; k<52; k++){ int r=k+rand()%(52-k); swap(cards[k] , cards[r]);}
+
+    for ( int i = 0 ; i < 52 ; i++ )
+        printf("%c%c\t", cards[i].figure(), cards[i].color());
+
+    printf("\nOto karty są\n");
 
 }
